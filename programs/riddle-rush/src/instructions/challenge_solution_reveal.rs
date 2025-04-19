7e661d6a7a2b6e3d037577cb04a2de169dc06157
banescusebi <seb@asymmetric.re>
2025-04-19 12:57:25 +0200

    Added revealing of the challenge solution

--- programs/riddle-rush/src/instructions/challenge_solution_reveal.rs
+++ programs/riddle-rush/src/instructions/challenge_solution_reveal.rs
@@ -1,1 +1,33 @@
 // The creator of the challenge reveals the solution to the challenge
+use anchor_lang::prelude::*;
+
+#[derive(Accounts)]
+pub struct ChallengeSolutionReveal<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+    #[account(
+        seeds = [b"challenge", id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub challenge: Account<'info, Challenge>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<ChallengeSolutionReveal>) -> Result<()> {
+    let challenge = &mut ctx.accounts.challenge;
+    require!(Clock::get()?.unix_timestamp > challenge.sumbmission_deadline, RiddleRushError::SolutionRevealDeadlineNotMet);
+
+    if challenge.solution == "" {
+        match evaluate_expression(&_question) {
+            Ok(result) => {
+                challenge.solution = result.to_string();
+            },
+            Err(e) => {
+                msg!("Error evaluating expression: {:?}", e);
+                return err!(RiddleRushError::InvalidExpression);
+            }
+        }
+    }
+
+    Ok(())
+}
